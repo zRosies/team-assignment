d4b6bad0196d0e92fb59e2d4b873fe8f5a490718
zRosies <gustavo.enrollment@gmail.com>
2023-11-13 18:43:42 -0300

    changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const employees = require('./employees');
 // const swagger = require('./swagger');
 // const user = require('./user')
-const storesRoute = require ('./stores')
+const storesRoute = require("./stores");
+const employee = require("./employees");
 
-router.use ('/stores', storesRoute)
+router.use("/stores", storesRoute);
+router.use("/employee", employee);
 
 module.exports = router;
