b8eeede3a44e4047c5488b34da68c95aa48c8ed3
elerunnduvictor <velerunndu@gmail.com>
2023-11-20 16:37:39 -0800

    updated routes and controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const storesRoute = require("./stores");
 const employee = require("./employees");
 const vehicle = require("./vehicle");
 const vehicleMaintenance = require("./vehicleMaintenance");
+const user = require("./users")
 
 router.use("/stores", storesRoute);
 router.use("/employee", employee);
 router.use("/vehicle", vehicle);
 router.use("/maintenance", vehicleMaintenance);
 router.use("/api-docs", swagger);
+router.use("/user", user);
 
 module.exports = router;
