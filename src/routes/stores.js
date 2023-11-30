4ce53179b723e3185c08b5f4235575c3a1521e17
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2023-11-30 15:50:00 -0500

    added auth to the stores routes

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,11 +1,12 @@
 const route = require("express").Router();
 const stores = require("../controllers/stores");
 const { validateStore, validate } = require("../validation/validator");
+const authenticate = require("../oauth/middleware");
 
 route.get("/", stores.getAllStores);
 route.get("/:id", stores.getStoreById);
-route.put("/:id", validateStore(), validate, stores.putStore);
-route.post("/", validateStore(), validate, stores.postStore);
-route.delete("/:id", stores.deleteStore);
+route.put("/:id", authenticate,  validateStore(), validate, stores.putStore);
+route.post("/", authenticate, validateStore(), validate, stores.postStore);
+route.delete("/:id", authenticate, stores.deleteStore);
 
 module.exports = route;
