e8f63b4d0419ef356a3cf516f0ae18c34ea3bbb5
zRosies <gustavo.enrollment@gmail.com>
2023-11-30 18:33:20 -0300

    final changes

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,22 +2,26 @@ const route = require("express").Router();
 const users = require("../controllers/user");
 const validator = require("../validation/validator");
 
+const authenticate = require("../oauth/middleware");
+
 route.get("/", users.getAllUsers);
 route.get("/:id", users.getUserById);
-route.delete("/:id", users.deleteUser);
+route.delete("/:id", authenticate, users.deleteUser);
 
 route.post(
-    "/",
-    validator.validateUser(),
-    validator.validate,
+  "/",
+  authenticate,
+  validator.validateUser(),
+  validator.validate,
 
-    users.createUser,
+  users.createUser,
 );
 route.put(
-    "/:id",
-    validator.validateUser(),
-    validator.validate,
-    users.updateUser,
+  "/:id",
+  authenticate,
+  validator.validateUser(),
+  validator.validate,
+  users.updateUser,
 );
 
 module.exports = route;
--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -4,20 +4,28 @@ const controllerVehicle = require("../controllers/vehicle");
 
 const validator = require("../validation/validator");
 
+const authenticate = require("../oauth/middleware");
 
 router.get("/", controllerVehicle.getVehicle);
 
 router.get("/:id", controllerVehicle.getVehicleById);
 
-router.post("/",
-    validator.validateVehicle(),
-    validator.validate,
-    controllerVehicle.createVehicle);
+router.post(
+  "/",
+  authenticate,
 
-router.put("/:id",
-    validator.validateVehicle(),
-    validator.validate,
-    controllerVehicle.updateVehicleById);
+  validator.validateVehicle(),
+  validator.validate,
+  controllerVehicle.createVehicle,
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  validator.validateVehicle(),
+  validator.validate,
+  controllerVehicle.updateVehicleById,
+);
 
 router.delete("/:id", controllerVehicle.deleteVehicleById);
 
